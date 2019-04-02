4aeff01f8cc0883cc16980d871ab2dad06495261
mrafiq709 <mrafiq709@gmail.com>
2019-04-02 13:24:38 +0700

    Basic View Initial Commit

---
+++ BasicView/BasicView/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BasicView
+{
+    public partial class MainPage : ContentPage
+    {
+        int cnt = 0;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        void OnButtonClicked(object sender, EventArgs e)
+        {
+            string str = Convert.ToString(cnt++);
+            (sender as Button).Text = "Click me again! " + str;
+            Console.WriteLine("Click me again! " + str);
+
+        }
+
+        void OnEntryTextChanged(object sender, TextChangedEventArgs e)
+        {
+            string oldText = e.OldTextValue;
+            string newText = e.NewTextValue;
+
+            Console.WriteLine("Entry Old Text: " + oldText);
+            Console.WriteLine("Entry New Text: " + newText);
+
+        }
+
+        void OnEntryCompleted(object sender, EventArgs e)
+        {
+            string text = ((Entry)sender).Text;
+            Console.WriteLine("Entry Text: " + text);
+        }
+
+        void OnEditorTextChanged(object sender, TextChangedEventArgs e)
+        {
+            string oldText = e.OldTextValue;
+            string newText = e.NewTextValue;
+
+            Console.WriteLine("Editor Old Text: " + oldText);
+            Console.WriteLine("Editor New Text: " + newText);
+        }
+
+        void OnEditorCompleted(object sender, EventArgs e)
+        {
+            string text = ((Editor)sender).Text;
+            Console.WriteLine("Editor Completed Text: " + text);
+        }
+    }
+}
