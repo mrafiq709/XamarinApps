bc8c51c0ec4361d2c6893a6ab612850df4ec20ef
mrafiq709 <mrafiq709@gmail.com>
2019-04-03 09:48:38 +0700

    ListView initial commit

---
+++ ListViewTutorial/ListViewTutorial.Android/obj/Debug/81/android/src/md581f92ff3ea319c37bcb1594ffa66aa81/MainActivity.java
@@ -0,0 +1,47 @@
+package md581f92ff3ea319c37bcb1594ffa66aa81;
+
+
+public class MainActivity
+	extends md51558244f76c53b6aeda52c8a337f2c37.FormsAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("ListViewTutorial.Droid.MainActivity, ListViewTutorial.Android", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("ListViewTutorial.Droid.MainActivity, ListViewTutorial.Android", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
---
+++ ListViewTutorial/ListViewTutorial/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ListViewTutorial
+{
+    public partial class MainPage : ContentPage
+    {
+        public IList<Monkey> Monkeys { get; private set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            string[] name, location, img;
+
+            name = new string[] { "Capuchin Monkey", "Blue Monkey", "Squirrel Monkey", "Golden Lion Tamarin", "Baboon" };
+
+            location = new string[] { "Central & South America", "Central and East Africa", "Central & South America", "Brazil", "Africa & Asia"};
+
+            img = new string[] { "http://upload.wikimedia.org/wikipedia/commons/thumb/4/40/Capuchin_Costa_Rica.jpg/200px-Capuchin_Costa_Rica.jpg",
+            "http://upload.wikimedia.org/wikipedia/commons/thumb/8/83/BlueMonkey.jpg/220px-BlueMonkey.jpg",
+            "http://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Saimiri_sciureus-1_Luc_Viatour.jpg/220px-Saimiri_sciureus-1_Luc_Viatour.jpg",
+             "http://upload.wikimedia.org/wikipedia/commons/thumb/8/87/Golden_lion_tamarin_portrait3.jpg/220px-Golden_lion_tamarin_portrait3.jpg",
+             "http://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Papio_anubis_%28Serengeti%2C_2009%29.jpg/200px-Papio_anubis_%28Serengeti%2C_2009%29.jpg"
+            };
+
+            Monkeys = new List<Monkey>();
+
+            for (int i = 0; i < name.Length; i++)
+            {
+                Monkeys.Add(new Monkey
+                {
+                    Name = name[i],
+                    Location = location[i],
+                    ImageUrl = img[i]
+                });
+            }
+
+            BindingContext = this;
+        }
+        void OnListViewItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            Monkey selectedItem = e.SelectedItem as Monkey;
+            Console.WriteLine("Selected: " + selectedItem.Name);
+        }
+
+        void OnListViewItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            Monkey tappedItem = e.Item as Monkey;
+            Console.WriteLine("Tapped: " + tappedItem.Name);
+        }
+    }
+}
