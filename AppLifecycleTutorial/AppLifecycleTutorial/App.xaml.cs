fec8e2c517e80e0b8f32fef710bc28a9e5b6a4b6
mrafiq709 <mrafiq709@gmail.com>
2019-04-09 15:51:27 +0700

    Initial commit

---
+++ AppLifecycleTutorial/AppLifecycleTutorial/App.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace AppLifecycleTutorial
+{
+    public partial class App : Application
+    {
+        const string displayText = "displayText";
+
+        public string DisplayText { get; set; }
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+            Console.WriteLine("OnStart");
+            if (Properties.ContainsKey(displayText))
+            {
+                DisplayText = (string)Properties[displayText];
+            }
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+            Console.WriteLine("OnSleep");
+            Properties[displayText] = DisplayText;
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+            Console.WriteLine("OnResume");
+        }
+    }
+}
---
+++ AppLifecycleTutorial/AppLifecycleTutorial/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppLifecycleTutorial
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            entry.Text = (Application.Current as App).DisplayText;
+        }
+
+        void OnEntryCompleted(object sender, EventArgs e)
+        {
+            (Application.Current as App).DisplayText = entry.Text;
+        }
+    }
+}
