deabe34c711654801fe6f3642bfe8dd4eefcde31
mrafiq709 <mrafiq709@gmail.com>
2019-04-16 11:30:40 +0700

    Initial commit

---
+++ LocalDatabaseTutorial/LocalDatabaseTutorial/App.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.IO;
+
+[assembly:XamlCompilation(XamlCompilationOptions.Compile)]
+namespace LocalDatabaseTutorial
+{
+    public partial class App : Application
+    {
+        static Database database;
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+        public static Database Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new Database(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "people.db3"));
+                }
+                return database;
+            }
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
