d6f33003659d5ffc28f772e2ac439401af96a5fa
mrafiq709 <mrafiq709@gmail.com>
2019-04-22 14:24:39 +0700

    File write and Read

---
+++ WorkingWithFileHandling/WorkingWithFileHandling/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace WorkingWithFileHandling
+{
+    public partial class MainPage : ContentPage
+    {
+        string fileName;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        
+        void OnButtonSave(object sender, EventArgs e)
+        {
+            Console.WriteLine("Click me again!\n");
+            //loadTxt.Text = enTxt.Text;
+            fileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "temp.txt");
+            Console.WriteLine("Path: " + fileName);
+            fileWriter(fileName, enTxt.Text);
+        }
+
+        void OnButtonLoad(object sender, EventArgs e)
+        {
+            Console.WriteLine("Loading Button !\n");
+
+            fileRead(fileName);
+        }
+
+        void fileWriter(String fName, string text)
+        {
+            File.WriteAllText(fName, text);
+        }
+        
+        void fileRead(String fName)
+        {
+            if (File.Exists(fName))
+            {
+                string text = File.ReadAllText(fName);
+                loadTxt.Text = text;
+                Console.WriteLine("File Content:  " + text);
+            }
+
+        }
+        
+    }
+}
